ef76fadc8caf1decc4d407b148972ac5733f3b25
adecuellarv <76180286+adecuellarv@users.noreply.github.com>
2024-10-22 21:43:43 -0600

    layout login

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 import Login from './pages/login/Login';
 import reportWebVitals from './reportWebVitals';
 
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import './styles.scss';
 
 const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Hola desde login</h1>
+    <div className='container-login'>
+      <div className='text-center'>
+        <h1>Crear cuenta</h1>
+      </div>
+      <div className='div-login'>
+        <div className='container-input'>
+          <label>Correo</label>
+          <input
+            type='email'
+            placeholder='Ej ade@gmail.com'
+          />
+        </div>
+        <div className='container-input'>
+          <label>Contraseña</label>
+          <input
+            type='password'
+            placeholder='*******'
+          />
+        </div>
+        <div className='container-input'>
+          <label>Repetir Contraseña</label>
+          <input
+            type='password'
+            placeholder='*******'
+          />
+        </div>
+        <div className='container-button'>
+          <div className='d-right'>
+            <button className='button b-blue'>Crear</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
